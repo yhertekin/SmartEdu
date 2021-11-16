a47a5e72aa3719b634b72d3e90c1f3ea5b170366
yhertekin <yhertekin@gmail.com>
2021-11-16 19:54:27 +0300

    add slug

--- models/Course.js
+++ models/Course.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
+
 const { Schema } = mongoose;
 
 const CourseSchema = new Schema({
@@ -15,6 +17,18 @@ const CourseSchema = new Schema({
 		type: Date,
 		default: Date.now,
 	},
+	slug: {
+		type: String,
+		required: true,
+	},
+});
+
+CourseSchema.pre("validate", function (next) {
+	this.slug = slugify(this.name, {
+		lower: true,
+		strict: true,
+	});
+	next();
 });
 
 const Course = mongoose.model("Course", CourseSchema);
