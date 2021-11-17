b9734f2dfc6470326950004ec5cbe65d9657e7bf
yhertekin <yhertekin@gmail.com>
2021-11-17 23:30:51 +0300

    add register page

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -20,13 +20,14 @@ exports.getAllCourses = async (req, res) => {
 	try {
 		const categorySlug = req.query.categories;
 		const category = await Category.findOne({ slug: categorySlug });
+		const categories = await Category.find();
 
 		let filter = {};
 		if (categorySlug) {
 			filter = { category: category._id };
 		}
 		const courses = await Course.find(filter);
-		const categories = await Category.find();
+
 		res.status(200).render("courses", {
 			courses,
 			categories,
