c015a60969208187617deec96caf671a9d7d7efc
yhertekin <yhertekin@gmail.com>
2021-11-18 00:40:30 +0300

    login user

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,5 +5,5 @@ const authController = require("../controllers/authController");
 const router = express.Router();
 
 router.route("/signup").post(authController.createUser); // users/signup
-
+router.route("/login").post(authController.loginUser); // users/login
 module.exports = router;
