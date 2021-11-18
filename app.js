7f3444769551bd6f7866658080b2167479877266
yhertekin <yhertekin@gmail.com>
2021-11-19 00:37:13 +0300

    add user session

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const session = require("express-session");
 
 const pageRoute = require("./routes/pageRoute");
 const courseRoute = require("./routes/courseRoute");
@@ -16,13 +17,26 @@ mongoose
 //Template Engine
 app.set("view engine", "ejs");
 
+//Global
+global.userIN = null;
+
 //Middlewares
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+	session({
+		secret: "my_keyboard_cat",
+		resave: false,
+		saveUninitialized: true,
+	})
+);
 
 //Routes
-
+app.use("*", (req, res, next) => {
+	userIN = req.session.userID;
+	next();
+});
 app.use("/", pageRoute);
 app.use("/courses", courseRoute);
 app.use("/categories", categoryRoute);
--- controllers/authController.js
+++ controllers/authController.js
@@ -19,15 +19,15 @@ exports.createUser = async (req, res) => {
 exports.loginUser = async (req, res) => {
 	try {
 		const { email, password } = req.body;
-		User.findOne({ email }, (err, user) => {
-			if (user) {
-				bcrypt.compare(password, user.password, (err, same) => {
-					if (same) {
-						res.status(200).send("You are logged in");
-					}
-				});
-			}
-		});
+		const user = await User.findOne({ email });
+		if (user) {
+			bcrypt.compare(password, user.password, (err, same) => {
+				if (same) {
+					req.session.userID = user._id;
+					res.status(200).redirect("/");
+				}
+			});
+		}
 	} catch (error) {
 		res.status(400).json({
 			status: "fail",
