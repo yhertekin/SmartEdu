00661c5bfca686538c43675d91b597c86f1e6bdb
yhertekin <yhertekin@gmail.com>
2021-11-19 01:27:06 +0300

    add user logout with mongo session control

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,4 +6,5 @@ const router = express.Router();
 
 router.route("/signup").post(authController.createUser); // users/signup
 router.route("/login").post(authController.loginUser); // users/login
+router.route("/logout").get(authController.logoutUser); // users/logout
 module.exports = router;
